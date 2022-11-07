e34136567e2706a2518d044e4dc98386cb7b084b
sada-george <108307073+sada-george@users.noreply.github.com>
2022-11-07 12:40:05 -0800

    feat: change build port

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
+app.set('port', process.env.PORT || 3100); // GİRİŞ PORTU AYARLANDI
 app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
 app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
 app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
 
-require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
+require('./app/routes')(app);
 
 /*
 
